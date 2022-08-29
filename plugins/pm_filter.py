7d22b60c36e898f4161532c8b9dd11c63f1ae3ca
JAGANVINOD <91742504+JAGANVINOD@users.noreply.github.com>
2022-08-29 12:08:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -45,7 +45,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda 🌝", show_alert=True)
+        return await query.answer("You have no Reason to Tap This 😄", show_alert=True)
     try:
         offset = int(offset)
     except:
